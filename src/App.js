1a71371f1188f6d168e09d1434b03d6aafe4e7b3
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-05-18 06:31:26 +0100

    Add score, highscore and chosenColors state and onclick for color divs

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ function App() {
     'aqua',
   ]);
 
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [chosenColors, setChosenColors] = useState([]);
+
+  const handleClick = (e) => {
+    if (chosenColors.includes(e.target.className)) {
+      if (score > highScore) setHighScore(score);
+      setScore(0);
+    } else {
+      setScore(score + 1);
+      setChosenColors(chosenColors.concat(e.target.className));
+    }
+  };
+
   return (
     <div>
       <Cards colors={colors} />
