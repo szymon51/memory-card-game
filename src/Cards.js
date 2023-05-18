f046bea685c76d584bddf11c66fb55d1c4e87c21
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-05-18 05:51:13 +0100

    Add function for generating random colored cards

--- src/Cards.js
+++ src/Cards.js
@@ -1,5 +1,33 @@
-const Cards = () => {
-  return <div>hello</div>;
+import './Cards.css';
+
+const Cards = ({ colors }) => {
+  const getRandomColors = (colors, colorAmount = 8) => {
+    const usedColors = [];
+
+    for (let i = 0; i < colorAmount; i++) {
+      let randomNumber = Math.floor(Math.random() * (colors.length - 1));
+      while (usedColors.includes(colors[randomNumber])) {
+        randomNumber = Math.floor(Math.random() * (colors.length - 1));
+      }
+      usedColors.push(colors[randomNumber]);
+    }
+    return usedColors;
+  };
+  const cardGenerator = (colors) => {
+    return getRandomColors(colors).map((randomColor) => {
+      return (
+        <div
+          style={{
+            backgroundColor: randomColor,
+            width: '200px',
+            height: '200px',
+          }}
+        ></div>
+      );
+    });
+  };
+
+  return <div className="grid">{cardGenerator(colors)}</div>;
 };
 
 export default Cards;
